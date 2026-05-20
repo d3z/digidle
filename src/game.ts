a746a84bb91730aa4d7624bbbdfdc1613e70df3f
Gareth Fleming <gareth.fleming@libertymutual.com>
2026-05-20 08:57:20 +0100

    initial commit

---
+++ src/game.ts
@@ -0,0 +1,45 @@
+enum DigitPlacement {
+    wrong,
+    misplaced,
+    correct
+};
+
+type Response = DigitPlacement[] | 'game_over';
+
+export class Game {
+    private answer: string;
+    private attempts: number;
+
+    constructor(answerNumber: number, private maxAttempts = 5) {
+        this.answer = `${answerNumber}`;
+        this.attempts = 0;
+    }
+
+    haveAttemptsLeft(): boolean {
+        return this.attempts <= this.maxAttempts;
+    }
+
+    makeAttempt(guess: number): Response {
+        if (this.haveAttemptsLeft()) {
+            this.attempts++;
+            return this.checkGuess(guess);
+        }
+        return 'game_over';
+    }
+
+    private checkGuess(guess: number): Response {
+        const response: Response= [];
+        const guessParts = `${guess}`.split('');
+        guessParts.forEach((next, idx) => {
+            const nextIdx = this.answer.indexOf(next);
+            if (nextIdx === idx) {
+                response.push(DigitPlacement.correct);
+            } else if (nextIdx === -1) {
+                response.push(DigitPlacement.wrong);
+            } else {
+                response.push(DigitPlacement.misplaced);
+            }
+        });
+        return response;
+    }
+}
---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import express, {type Request, type Response} from 'express';
+import { Game } from './game.ts';
+
+const app = express();
+app.use(express.json());
+
+const games: Map<number, Game> = new Map<number, Game>();
+let nextGameId = 0;
+
+app.get('/new', (_: Request, res: Response) => {
+    const randomAnswer = Math.floor(Math.random() * (2026 - 1900 + 1)) + 1900;
+    const game = new Game(randomAnswer);
+    games.set(nextGameId, game);
+    res.json({id: nextGameId++});
+});
+
+app.put('/:id/:guess', (req: Request, res: Response) => {
+    const {id, guess} = req.params;
+    if (typeof id !== 'string') {
+        res.status(400).json({error: 'Invalid game id'});
+        return;
+    }
+    if (typeof guess !== 'string') {
+        res.status(400).json({error: 'Invalid guess. Must be a number.'});
+        return;
+    }
+    const gameId = parseInt(id);
+    if (isNaN(gameId)) {
+        res.status(400).json({error: 'Invalid game id'});
+        return;
+    }
+    console.log(games);
+    if (!games.has(gameId)) {
+        res.status(404).json({error: 'Game not found'});
+    }
+    const game = games.get(gameId);
+    const guessNumber = parseInt(guess);
+    const response = game?.makeAttempt(guessNumber);
+    res.json({response});
+});
+
+app.listen(3000, () => {
+    console.log('Digidle server running on localhost:3000');
+});
